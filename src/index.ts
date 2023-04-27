0b296bb1bb1e1d7e3b8386fffee381220bc886f0
Amal Mohan N <amal.mohan@diagnal.com>
2023-04-27 15:26:17 +0530

    lightning build

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
-import { Launch } from '@lightningjs/sdk';
-import { App } from './App';
+import { Lightning, Launch, PlatformSettings, AppData } from '@lightningjs/sdk'
+import { App } from './App.js'
 
-Launch(App, {
-  stage: {
-    canvas: document.getElementsByTagName('canvas')[0],
-  },
-  debug: true,
-  enablePointer: true,
-  keys: {
-    38: 'Up',
-    40: 'Down',
-    37: 'Left',
-    39: 'Right',
-    13: 'Enter',
-    8: 'Back',
-    27: 'Exit',
-  },
-}, { /* Platform settings */ });
+export default function (
+  appSettings: Lightning.Application.Options,
+  platformSettings: PlatformSettings,
+  appData: AppData,
+) {
+  return Launch(App, appSettings, platformSettings, appData)
+}
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ command, mode, ssrBuild }) => {
   return {
     build: {
-      minify: true,
+      minify: false,
       sourcemap: true,
     },
   }
